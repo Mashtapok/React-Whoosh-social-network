5e5e4491729b43099cd37b27b389d2ea43d651a3
Stepan <gowxking@gmail.com>
2020-05-11 21:42:18 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <HashRouter basename={process.env.PUBLIC_URL}>
                 <Provider store={store}>
                     <App/>
                 </Provider>
-            </BrowserRouter>
+            </HashRouter>
         , document.getElementById('root')
     );
 };
